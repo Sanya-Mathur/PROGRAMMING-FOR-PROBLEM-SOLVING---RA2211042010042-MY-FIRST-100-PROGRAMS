cbbb2e2b216329f5c1d10729783d1ad5999f9681
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 19:17:19 +0530

    Update and rename swap numbers without third variable to swap_numbers_without_third_variable.c

--- swap_numbers_without_third_variable.c
+++ swap_numbers_without_third_variable.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 main() 
-{
+ {
+    printf("RA2211042010042\n");
   int number1, number2;
   printf("\nEnter values for number1 and number2: ");
   scanf("%d%d",& number1,& number2);
+  printf("\nBefore swapping value of number1=%d number2=%d", number1, number2);
   number1= number1+ number2;
   number2= number1- number2;
   number1= number1- number2;
-  printf("\nBefore swapping value of number1=%d number2=%d", number1, number2);
+  
   printf("\nAfter swapping value of number1=%d and number2=%d", number1, number2);
 } 
