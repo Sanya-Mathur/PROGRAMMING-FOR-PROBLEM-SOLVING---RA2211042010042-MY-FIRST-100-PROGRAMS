3fcd7d62489086c1bea63cd8505fc55254f96c92
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:54:00 +0530

    Update and rename to display array elements to floor_number.c

---
+++ floor_number.c
@@ -0,0 +1,16 @@
+
+#include <math.h>
+#include <stdio.h>
+void main() 
+{  printf("RA2211042010042\n");
+  float number;
+  int floor_number;
+   printf("Enter number: ");
+  scanf("%f",&number);
+  
+  floor_number=floor(number);
+ 
+  printf("floor number= %d\n",floor_number);
+ 
+}
+
