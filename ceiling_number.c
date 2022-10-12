d40bee3d0ffb1994fe83ad2cd24af9cbeca0872c
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:52:30 +0530

    Update and rename transpose of matrix to ceiling_number.c

---
+++ ceiling_number.c
@@ -0,0 +1,16 @@
+
+#include <math.h>
+#include <stdio.h>
+void main() 
+{  printf("RA2211042010042\n");
+  float number;
+  int ceiling;
+   printf("Enter number: ");
+  scanf("%f",&number);
+  
+  ceiling=ceil(number);
+ 
+  printf("ceiling number= %d\n",ceiling);
+ 
+}
+
