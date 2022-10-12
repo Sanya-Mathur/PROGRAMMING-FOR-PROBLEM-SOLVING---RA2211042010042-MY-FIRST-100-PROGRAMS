e9d22d9b4b5ed8b7ccbee20b95a12b2b7e0c5292
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:56:09 +0530

    Update and rename sum of array elements to cuberoot.c

---
+++ cuberoot.c
@@ -0,0 +1,17 @@
+
+#include <math.h>
+#include <stdio.h>
+void main() 
+{  printf("RA2211042010042\n");
+  float number;
+  float cube_root;
+  
+   printf("Enter number: ");
+  scanf("%f",&number);
+  
+  cube_root=cbrt(number);
+ 
+  printf("cube root= %f\n",cube_root);
+ 
+}
+
