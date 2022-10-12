9cdd4e91370e63f261d340d822195e63cd15c5f8
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 16:52:41 +0530

    Update and rename Even number to Even_number.c

--- Even_number.c
+++ Even_number.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 main() 
-{
+{ 
+  printf("RA2211042010042\n");
   int number;
   printf("Enter the value of number: ");
   scanf("%d",&number);
@@ -9,5 +10,7 @@ main()
     printf("%d is even number.", number);
   }
 else
-{printf(“%d is odd number .”,number);}
+{
+  printf(“%d is odd number .”,number);
+}
 } 
