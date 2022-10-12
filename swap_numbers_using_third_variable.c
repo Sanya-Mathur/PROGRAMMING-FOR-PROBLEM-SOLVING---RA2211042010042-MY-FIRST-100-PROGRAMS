e92bdfdc23a7449828f311823cb56865342ab386
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 19:10:44 +0530

    Update and rename swap numbers using third variable to swap_numbers_using_third_variable.c

--- swap_numbers_using_third_variable.c
+++ swap_numbers_using_third_variable.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-int main() 
-{
+ main() 
+
+{ printf("RA2211042010042\n");
+ 
   int number1, number2, temporary_variable;
   printf("\nEnter values for number1 and number2: ");
   scanf("%d%d",& number1,& number2);
   temporary_variable = number1;
   number1= number2;
   number2= temporary_variable;
-  printf("\nBefore swap value of number1=%d and number2=%d", number1, number2);
+  printf("\nBefore swap value of number1=%d and number2=%d", number2, number1);
   printf("\nAfter swap value of number1=%d and number2=%d", number1, number2);
-return 0;
 } 
 
 
