f98fd3b501a081f984d09ddde4ad1ec7abd6290d
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:23:53 +0530

    Update and rename display contents of a file to product_of_two_numbers.c

---
+++ product_of_two_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    printf("RA2211042010042\n");
+  int number1,number2;
+  printf("Enter value for number1: ");
+  scanf("%d",&number1);
+  printf("Enter value for number2: ");
+  scanf("%d",&number2 );
+  int product;
+  product=number1* number2;
+  printf("product=%d", product );
+
+
+return 0;
+}
