79f62a4fe8fb9fddaaddc2bed99ca5c4a710e68b
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:19:20 +0530

    Update and rename call by reference to difference_of_two_numbers.c

---
+++ difference_of_two_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    printf("RA2211042010042\n");
+  int number1,number2;
+  printf("Enter value for number1: ");
+  scanf("%d",&number1);
+  printf("Enter value for number2: ");
+  scanf("%d",&number2 );
+  int difference;
+   difference=number1- number2;
+  printf("difference=%d", difference );
+  
+return 0;
+}
