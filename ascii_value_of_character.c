e3f80f632c910afe272cd30179ba548a2818e300
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 19:01:16 +0530

    Update and rename ascii value of character to ascii_value_of_character.c

---
+++ ascii_value_of_character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("RA2211042010042\n");
+    char character;
+    printf("Enter a character : ");
+    scanf("%c" , &character);
+    printf("\n\nASCII value of %c = %d",character,character);
+
+    
+    return 0;
+}
