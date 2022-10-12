eafff11cccd883ee2ae801572fcd2536be6167f9
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:17:37 +0530

    Update and rename arithmetic operators to sum_of_two_numbers.c

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    printf("RA2211042010042\n");
+  int number1,number2;
+  printf("Enter value for number1: ");
+  scanf("%d",&number1);
+  printf("Enter value for number2: ");
+  scanf("%d",&number2 );
+  int sum;
+  sum=number1+ number2;
+  
+  printf("sum=%d", sum );
+  
+
+return 0;
+}
