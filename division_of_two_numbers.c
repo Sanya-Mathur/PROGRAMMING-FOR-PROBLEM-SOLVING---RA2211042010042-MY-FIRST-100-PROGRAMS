5fc89a452e40f4d4b75f5531a571ff0874bb3190
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-12 18:21:52 +0530

    Update and rename arrays of structure to division_of_two_numbers.c

---
+++ division_of_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    printf("RA2211042010042\n");
+  int number1,number2;
+  printf("Enter value for number1: ");
+  scanf("%d",&number1);
+  printf("Enter value for number2: ");
+  scanf("%d",&number2 );
+  int quotient;
+  quotient=number1/ number2;
+  
+  printf("quotient=%d", quotient );
+
+return 0;
+}
+                 
