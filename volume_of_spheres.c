331350b290d5adf4275091cdb6ed817b9eab0475
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-13 20:04:41 +0530

    Update and rename using getchar() and putchar() to volume_of_spheres.c

---
+++ volume_of_spheres.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{ printf("RA2211042010042\n");
+  float radius;
+  printf("enter radius of sphere");
+  scanf("%f",&radius);
+  float volume=1.33*3.14*radius*radius;
+  printf("The volume=%f",volume);
+}
