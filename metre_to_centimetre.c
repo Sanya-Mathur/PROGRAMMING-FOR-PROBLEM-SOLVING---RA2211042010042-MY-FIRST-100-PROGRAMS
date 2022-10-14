034d5034c830d2235ed5be09aa0cd455425fe49a
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 16:10:32 +0530

    Update and rename decimal to octal to metre_to_centimetre.c

---
+++ metre_to_centimetre.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   
+  printf("RA2211042010042\n");
+ 
+    float cm, m;
+    printf("Enter length in m: ");
+    scanf("%f", &m);
+    cm= m*100;
+    printf("Equivalent length in cm = %fcm", cm);
+    return 0;
+}
