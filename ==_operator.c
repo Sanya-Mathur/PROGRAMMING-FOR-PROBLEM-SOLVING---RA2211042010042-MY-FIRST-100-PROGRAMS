f35e48adda15c3b707cd0852b9787bb4da92ee6f
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 14:52:22 +0530

    Update and rename pointer to an array of integers to ==_operator.c

---
+++ ==_operator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int main () 
+{
+   printf("RA2211042010042\n");
+  
+   int a = 50;
+ 
+   
+   if( a == 10 ) 
+   {
+     
+      printf("a is 10\n" );
+   } 
+  else if( a == 20 ) 
+  {
+     
+      printf("a is 20\n" );
+   } 
+  else if( a == 30 )
+  {
+     
+    
+      printf("a is 30\n" );
+   } else
+  {
+    
+    
+      printf("No match\n" );
+   }
+   
+  
+   return 0;
+}
