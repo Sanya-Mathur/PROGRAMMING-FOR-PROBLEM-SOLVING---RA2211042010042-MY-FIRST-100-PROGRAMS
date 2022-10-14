5ecb574dfc022fa6daac6a368df5887c8c2efc7f
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 15:21:58 +0530

    Update and rename check prime or armstrong to ternary_operator.c

---
+++ ternary_operator.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() 
+{ 
+printf("RA2211042010042\n");
+  int number;
+
+  
+  printf("Enter your number: ");
+  scanf("%d", &number);
+
+  (number%2==0) ? printf("even") : printf("odd");
+
+  return 0;
+}
