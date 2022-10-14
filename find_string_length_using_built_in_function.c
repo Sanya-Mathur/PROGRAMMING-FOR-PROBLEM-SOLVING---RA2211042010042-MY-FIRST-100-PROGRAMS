b046b1aef818d964a4475058d77c8a970e752e09
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 17:18:21 +0530

    Update and rename find string length using built in function to find_string_length_using_built_in_function.c

---
+++ find_string_length_using_built_in_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+main() 
+{ printf("RA2211042010042\n");
+  int len1, len2;
+  char name[] = "SANYA";
+  len = strlen(name) ;
+
+  printf("\nString = %s length = %d", name, len);
+
+}
+
+
+
+
+
+
+
+
+
+
+
