36722944438ebe1b6caa500e6b3c02918f9831be
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 14:15:40 +0530

    Update and rename reverse string to average_of_three_terms.c

---
+++ average_of_three_terms.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+main() 
+{
+  printf("RA2211042010042\n");
+  int term1;
+  int term2;
+  int term3;
+  float average;
+  printf("Enter the 3 terms");
+  scanf("%d",&term1);
+  scanf("%d",&term2);
+  scanf("%d",&term3);
+  average=(term1+term2+term3)/3;
+  printf("Average=%f",average);
+  
+}
+
+
+
+
+
+
+
