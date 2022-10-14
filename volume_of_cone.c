c648a21b94aa37d70b4856c1a96c85c33622660d
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 16:32:18 +0530

    Update and rename function with return type to volume_of_cone.c

---
+++ volume_of_cone.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{ printf("RA2211042010042\n");
+  float radius;
+ float height;
+  printf("enter radius of cone");
+  scanf("%f",&radius);
+  float volume=0.33*3.14*radius*radius*radius;
+  printf("The volume=%f",volume);
+}
