cf904cb4329ec8fa6505c08121d5195f986f4b43
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 17:03:59 +0530

    Update and rename full pyramid to float_and_double.c

---
+++ float_and_double.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+printf("RA2211042010042\n");
+   float float_number=1.009999999995;
+   double double_number=1.0066666666667;
+   printf("%f",float_number);
+   printf("%lf",double_number);
+}
