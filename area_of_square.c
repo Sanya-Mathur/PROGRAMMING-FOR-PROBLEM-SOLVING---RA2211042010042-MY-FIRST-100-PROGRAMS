16d34a147f5959cd4c5550ccc00f50775488eda3
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 15:31:28 +0530

    Update and rename call by reference program to area_of_square.c

---
+++ area_of_square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010042\n");
+  
+  float side, area;
+
+ 
+  printf("Enter Side");
+  scanf("%f",&side);
+
+ 
+ 
+  area =side * side ;
+  printf("Area of square is: %f",area);
+
+  return 0;
+}
