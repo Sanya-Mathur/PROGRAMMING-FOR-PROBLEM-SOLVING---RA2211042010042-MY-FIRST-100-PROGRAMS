1931a885982a9c51e69ca565b6c4450f3de152c6
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 16:05:19 +0530

    Update and rename creating copy of string to centimetre_to_metre.c

---
+++ centimetre_to_metre.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   
+  printf("RA2211042010042\n");
+ 
+    float cm, m;
+    printf("Enter length in cm: ");
+    scanf("%f", &cm);
+    m= cm/100;
+    printf("Equivalent length in m = %fm", m);
+    return 0;
+}
