18c65e455ac6c954a9ff4e6d70fae6c99c8d59fa
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 15:54:51 +0530

    Update and rename concatenation of strings using built in function to celsius_to_fahrenheit.c

---
+++ celsius_to_fahrenheit.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010042\n");
+ 
+    float fahrenheit, celsius;
+    printf("Enter Temperature Value (in Celsius):\n");
+    scanf("%f", &celsius);
+    fahrenheit=(celsius*1.8)+32;
+    printf(" fahrenheit= %f", fahrenheit);
+    return 0;
+}
