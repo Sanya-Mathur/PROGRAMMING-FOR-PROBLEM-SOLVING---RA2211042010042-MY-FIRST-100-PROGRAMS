2f85a67d002335fca90154f59601ba2954f2dd24
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 16:35:06 +0530

    Update and rename integer as sum of primes to area_of_rectangle.c

---
+++ area_of_rectangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{ printf("RA2211042010042\n");
+  float length,breadth;
+  printf("enter length,&breadth");
+  scanf("%f",&length,&breadth);
+  float area=length*breadth;
+  printf("The area=%f",area);
+}
