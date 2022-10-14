d61d40a486882e28206c5ce04118dc58d2e8fa59
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-14 16:09:13 +0530

    Update and rename data types in c to int_data_types_in_c.c

---
+++ int_data_types_in_c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+main() 
+{  
+  printf("RA2211042010042\n");
+  int a;
+  long int b; 
+  short int c; 
+  unsigned int d;
+  a = 1;
+  b = 2;
+  c = 3;
+  d = 4;
+  
+
+
+  printf("\na = %d",a);
+  printf("\nb = %1d",b); 
+  printf("\nc = %d",c);
+  printf("\nd = %u",d);
+}
+
